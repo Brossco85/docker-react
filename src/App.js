056e7c519aa9aa3fa7ff8e13d8416865fe4569aa
Bobby Ross <bobbyross@ip-192-168-8-24.eu-west-2.compute.internal>
2019-01-17 17:26:21 +0000

    chore: test for martin

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Time to move to production
+            Time to move to production - for martin
           </p>
           <a
             className="App-link"
